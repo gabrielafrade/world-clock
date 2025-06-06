401e8faad6a419c0c19c6b574755497cbcd6a42c
Gabriela Frade <gabrielafrade@msn.com>
2025-06-06 19:01:14 +0100

    added js: created function for date and timezone

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function updateTime() {
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+  let madridTime = moment().tz("Europe/Madrid");
+
+  madridDateElement.innerHTML = madridTime.format("MMMM Do, YYYY");
+  madridTimeElement.innerHTML = madridTime.format("HH:mm:ss");
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do, YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format("HH:mm:ss");
+}
+updateTime();
+setInterval(updateTime, 1000);
